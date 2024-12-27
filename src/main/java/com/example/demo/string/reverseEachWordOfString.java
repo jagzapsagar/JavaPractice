d659018cbfef4fe90e69a690bb39fc721318949a
StoryOnJava <jagzapsagar228@gmail.com>
2024-12-27 22:53:53 +0530

    ReverStringjava8

--- src/main/java/com/example/demo/string/reverseEachWordOfString.java
+++ src/main/java/com/example/demo/string/reverseEachWordOfString.java
@@ -7,9 +7,16 @@ import java.util.stream.Collectors;
 public class reverseEachWordOfString {
 	public static void main(String[] args) {
 		String str = "Yeola is my city";
+		
+		//Reverse each word
 		String[] split = str.split(" ");
 		List<String> li = Arrays.stream(split).map(e -> new StringBuilder(e).reverse().toString()).collect(Collectors.toList());
 		System.out.println(li);
+		
+		//reverse String noy by alphaate
+		
+		String string = Arrays.stream(str.split(" ")).reduce((a,b) -> b +" "+a).toString();
+		System.out.println(string);
 	}
 	
 	
